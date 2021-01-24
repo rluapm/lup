47f1b606d163d2c57c8507677cccefd88638891d
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-01-24 18:33:27 +0100

    first packages

---
+++ packages/example.lua
@@ -0,0 +1,9 @@
+-- This is an example code.
+
+local mo = {}
+
+mo.print = function(m)
+    print(m)
+end
+
+return mo
---
+++ packages/float.lua
@@ -0,0 +1,8 @@
+local mo = {}
+
+mo.toFloat = function(iint)
+    iint = iint + 0.00
+    return iint
+end
+
+return mo
