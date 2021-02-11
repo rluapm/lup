ab54d35bde8a6e940537e4c5f6198e4086937421
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-02-11 21:32:39 +0100

    Update os.lua

--- packages/os.lua
+++ packages/os.lua
@@ -1,20 +1,5 @@
 local m = {}
 
-function m:GetSiblings(obj)
-	local siblings = {}
-	if obj == "self" then
-		for i,v in pairs(script.Parent:GetChildren()) do
-			table.insert(siblings,v)
-		end
-		return siblings
-	else
-		for i,v in pairs(obj.Parent:GetChildren()) do
-			table.insert(siblings,v)
-		end
-		return siblings
-	end
-end
-
 function m:Error(plr, err, code)
   local c = code or 1
 
