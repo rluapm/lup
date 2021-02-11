d0f947eeb818242d51e2f34a5b656d56c5eb1e55
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-02-11 20:45:17 +0100

    error and dup

--- packages/os.lua
+++ packages/os.lua
@@ -11,16 +11,46 @@ end
 function m:GetSiblings(obj)
 	local siblings = {}
 	if obj == "self" then
-	for i,v in pairs(script.Parent:GetChildren()) do
-		table.insert(siblings,v)
+		for i,v in pairs(script.Parent:GetChildren()) do
+			table.insert(siblings,v)
+		end
+		return siblings
+	else
+		for i,v in pairs(obj.Parent:GetChildren()) do
+			table.insert(siblings,v)
+		end
+		return siblings
 	end
-	return siblings
-else
-	for i,v in pairs(obj.Parent:GetChildren()) do
-		table.insert(siblings,v)
-	end
-	return siblings
 end
+
+function m:Error(plr, err, code)
+  local c = code or 1
+
+  plr:Kick('[ERROR: '..code..'] '..err)  
+end
+
+function m:GetEnv(obj)
+	local bleh = obj or "self"
+
+	if bleh == "self" then bleh = script end
+	
+	local fullpath
+	fullpath = bleh:GetFullName()
+
+	local env = fullpath:split('.')
+	
+	return env[1]
+end
+
+function m:Dup(obj, parent)
+  obj = obj or "Self"
+  parent = parent or script.Parent
+  if obj == "Self" then
+    obj = script
+  end
+  wait(0.01)
+  local newobject = obj:Clone()
+  newobject.Parent = parent
 end
 
 function m.path(obj)
